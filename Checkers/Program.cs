d53a026bc8c6e948312102e220f81c547b23b03f
Vladislav <bisnesbrend@gmail.com>
2022-07-18 20:15:26 +0300

    Создано игровое поле

---
+++ Checkers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers
+{
+    class Program
+    {
+        static int[,] map =
+        {
+            {0, 1, 0, 1, 0, 1, 0, 1 },
+            {1, 0, 1, 0, 1, 0, 1, 0 },
+            {0, 1, 0, 1, 0, 1, 0, 1 },
+            {0, 0, 0, 0, 0, 0, 0, 0 },
+            {0, 0, 0, 0, 0, 0, 0, 0 },
+            {2, 0, 2, 0, 2, 0, 2, 0 },
+            {0, 2, 0, 2, 0, 2, 0, 2 },
+            {2, 0, 2, 0, 2, 0, 2, 0 }
+        };
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
