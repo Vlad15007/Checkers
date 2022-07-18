9b42bb29315ac6514bb0ecc1ed9da50c65272de0
Vladislav <bisnesbrend@gmail.com>
2022-07-18 20:34:17 +0300

    Добавили шашки, сделали курсор

--- Checkers/Program.cs
+++ Checkers/Program.cs
@@ -19,9 +19,54 @@ namespace Checkers
             {0, 2, 0, 2, 0, 2, 0, 2 },
             {2, 0, 2, 0, 2, 0, 2, 0 }
         };
+
+        static int[] cursor = { 7, 7 };
+
+        static int GetCursor(string xy)
+        {
+            if (xy == "x") return cursor[0];
+            else if (xy == "y") return cursor[1];
+            else return -1;
+        }
+
+        static void DrawField()
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                Console.WriteLine("------------------------");
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if(GetCursor("x") == i && GetCursor("y") == j)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.Write(">");
+                    }
+                    else Console.Write(" ");
+
+
+                    if(map[i, j] == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.Write("o|");
+                    }
+                    else if (map[i, j] == 2)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write("o|");
+                    }
+                    else Console.Write(" |");
+
+                    Console.ResetColor();
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("------------------------");
+        }
+
         static void Main(string[] args)
         {
-
+            DrawField();
+            Console.ReadKey();
         }
     }
 }
